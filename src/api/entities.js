ed32dee4992eb4c68a1967b63afed0a66b0672ad
lucaspascoa <149783874+lucaspascoa@users.noreply.github.com>
2025-08-19 10:39:05 -0300

    Create entities.js

---
+++ src/api/entities.js
@@ -0,0 +1,25 @@
+import { base44 } from './base44Client';
+
+
+export const Product = base44.entities.Product;
+
+export const Category = base44.entities.Category;
+
+export const StoreConfig = base44.entities.StoreConfig;
+
+export const Order = base44.entities.Order;
+
+export const DeliveryZone = base44.entities.DeliveryZone;
+
+export const Staff = base44.entities.Staff;
+
+export const DailyClosure = base44.entities.DailyClosure;
+
+export const AuditLog = base44.entities.AuditLog;
+
+export const Role = base44.entities.Role;
+
+
+
+// auth sdk:
+export const User = base44.auth;
